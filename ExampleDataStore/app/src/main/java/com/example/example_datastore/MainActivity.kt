259e79c7d61b4e15164cc7e0aace7b15b6df4f07
Elif OKUMUŞ <98635931+utopica@users.noreply.github.com>
2024-09-01 20:38:34 +0300

    initial datastore example

---
+++ ExampleDataStore/app/src/main/java/com/example/example_datastore/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.example_datastore
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.example_datastore.ui.theme.Example_DataStoreTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Example_DataStoreTheme {
+
+                Anasayfa()
+            }
+        }
+    }
+}
+
+@Composable
+fun Anasayfa() {
+
+}
