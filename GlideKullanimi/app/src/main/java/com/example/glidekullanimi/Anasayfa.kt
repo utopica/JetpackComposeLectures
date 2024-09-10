915fd17f2dd3bf818a66fd95694b222ceba9d0c7
Elif OKUMUŞ <98635931+utopica@users.noreply.github.com>
2024-09-10 20:21:27 +0300

    workmanager initial

--- GlideKullanimi/app/src/main/java/com/example/glidekullanimi/Anasayfa.kt
+++ GlideKullanimi/app/src/main/java/com/example/glidekullanimi/Anasayfa.kt
@@ -4,18 +4,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.skydoves.landscapist.glide.GlideImage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Anasayfa(){
     //http://kasimadalan.pe.hu/yemekler/resimler/baklava.png
+
+    val resimAdi = remember { mutableStateOf("kofte.png") }
+
 
     Scaffold (
         topBar = { TopAppBar(title = { Text(text = "Anasayfa") }) }
@@ -23,11 +32,27 @@ fun Anasayfa(){
         paddingValues ->
 
         Column (
-            modifier = Modifier.fillMaxSize().padding(paddingValues),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
+            val url = "http://kasimadalan.pe.hu/yemekler/resimler/${resimAdi.value}"
 
+            GlideImage(imageModel = url, modifier= Modifier.size(200.dp, 200.dp))
+
+            Button(onClick = {
+                resimAdi.value = "baklava.png"
+            }) {
+                Text(text = "Resim 1")
+            }
+
+            Button(onClick = {
+                resimAdi.value = "ayran.png"
+            }) {
+                Text(text = "Resim 2")
+            }
         }
 
     }
